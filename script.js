e3e4f4adf624e09b01ff6692d5a94a1760955453
Follett <clf9008@gmail.com>
2020-11-27 15:13:12 -0700

    updated script.js with more fluid Javascript and added an alert if password is not proper length

--- script.js
+++ script.js
@@ -1,155 +1,125 @@
-// Assignment Code
-//To do: 
-    //logic: create a random string of characters that are unique and do not duplicate
-    //create an array for special characters
-    //create an array for numbers
-    //create an array for uppercase letters
-    //create an array for lowercase letters 
-    //create prompts for different criteria: how many characters, confirm you want numbers, uppercase and lowercase 
-        //-prompts will have if/else statements (if they click ok on special characters it goes to that array, if cancel do not include array)
-    //create functions for randomizing items, function for generating the password, function for prompting the users
-    var generateBtn = document.querySelector("#generate");
+//declaring variable for global memory for each of the possible choices for the users passwords
+var enter;
+var confirmNumber;
+var confirmChar;
+var confirmUpper;
+var confirmLower;
+//declaring an array for global memory that contains all the characters that could be generated in the password
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+//declaring an array for global memory that contains all the numbers that could be generated in the password
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//declaring an array for global memory that contains all the letters that could be generated in the password
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Uppercase is to convert letters to uppercase
+upperCase = [];
+//Choices declared outside the if statement so they can be concatenated upon condition
+var choices;
+//declaring a variable into global memory that has a function that will take a letter and make it Uppercase
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+//declaring a variable to global memory that coverts chosen letters to uppercase
+alpha2 = alpha.map(toUpper);
+//variable to create a generate button and appending that to the HTML document
+var get = document.querySelector("#generate");
+//Declaring an 'on click' function that listns to the 'generate' button and runs ths generatePassword funtion when clicked
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
 
-    //Creates and array of special characters to be used in our password if truthy
-    var specialChars = [
-        '@',
-        '%',
-        '+',
-        '\\',
-        '/',
-        "'",
-        '!',
-        '#',
-        '$',
-        '^',
-        '?',
-        ':',
-        ',',
-        ')',
-        '(',
-        '}',
-        '{',
-        ']',
-        '[',
-        '~',
-        '-',
-        '_',
-        '.'
-    
-    ];
-    
-    
-    
-    //Creates an array of lower case letters to be generated in our password if truthy
-    var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    //Creates an array of upper case letters to be generated in our password if truthy 
-    var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
-    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    //creates an array of numbers to be generated in our password if truthy
-    var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    //creates a variable to put into global memory of the total number of characters of the password which can be between 8 and 128
-    
-    
-    
-    //randomizing function 
-    function gettingRandomChar(array) {
-        //grab random index from selected array
-        var randomIndex = Math.floor(Math.random() * array.length);
-        var randomChar = array[randomIndex];
-      
-    
-        return randomChar;
-        
-    };
-    
-    //function to generate password based on conditional statements regarding password choice 
-    function usersPWOptions() {
-        
-    //generates confirm box which takes in true or false whether we want to use special characters in our password that we generate
-    var confirmSpecialChar = confirm("Do you want to use special characters in your password?");
-    //generates confirm box which takes in true or false whether we want to use numbers in our password
-    var confirmNumbers = confirm("Do you want to use numbers numbers in your password?");
-    //generates confirm box which takes in true or falase whether we want to use uppercase letters in our password
-    var confirmUpperCase = confirm("Do you want to use uppercase letters in your password?");
-    //generataes confirm box which takes in true or false whether we want to use lowercase letters in our password
-    var confirmLowerCase = confirm("Do you want to use lowercase letters in your password?");
-    //genereates a prompt box which ask the user how many charachters long they would like the password
-    var confirmStrlength = prompt("Enter a password length of 8 to 128?")
+//function to generate the password with the values chosen by the user
+function generatePassword() {
+    //Prompt to asking user how many characters they would like there password to be
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    //if no value is entered, alert the user the followuing 
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+        //if the value is below 8 or above 128 characters, prompt the user to chose a value in that range
+        enter = parseInt(prompt("You must choose between 8 and 128"));
 
-    console.log(confirmSpecialChar);
-    console.log(confirmNumbers);
-    console.log(confirmUpperCase);
-    console.log(confirmLowerCase);
-    //obj to store users choices
-    var userInput ={
-        confirmStrlength: confirmStrlength, 
-        confirmSpecialChar: confirmSpecialChar,
-        confirmNumbers: confirmNumbers,
-        confirmUpperCase: confirmUpperCase,
-        confirmLowerCase: confirmLowerCase,
-    
+    } else {
+        //once the correct number of characters for the password is chosen, run the following prompt for Special Characters, Numbers, Uppercase, and Lowercase
+        confirmNumber = confirm("Will this contain numbers?");
+        confirmChar = confirm("Will this contain special characters?");
+        confirmUpper = confirm("Will this contain Uppercase letters?");
+        confirmLower = confirm("Will this contain Lowercase letters?");
+    };
+
+    //If the user does not chose any values, run the following alert
+    if (!confirmChar && !confirmNumber && !confirmUpper && !confirmLower) {
+        choices = alert("You must choose a criteria!");
+
     }
-    return userInput;
+    //else-if statements if the user choses all four confirm variables
+    else if (confirmChar && confirmNumber && confirmUpper && confirmLower) {
+
+        choices = character.concat(number, alpha, alpha2);
     }
-    
-    //func to generate password based on the users choices
-    function createPassword() {
-        var userOptions = usersPWOptions();
-        //store pw as it's being created
-        var chosenPW =[]
-    
-    // Array to store types of characters to include in password
-    var characterChoice = [];
-    
-    // Array to contain one of each type of chosen character to make sure each will be used
-    var charactersToBeUsed = [];    
-    
-    //if the boleon statement on confirmSpecialChar is truthy, run the following funtion/method
-    if(userOptions.confirmSpecialChar === true) {
-     characterChoice=characterChoice.concat(specialChars);
-        charactersToBeUsed.push(gettingRandomChar(specialChars));
-    };
-    console.log(characterChoice)
-    //if the boleon statement on confirmNumbers is truthy, run the following function/method
-    if(userOptions.confirmNumbers === true) {
-        characterChoice=characterChoice.concat(numbers);
-        charactersToBeUsed.push(gettingRandomChar(numbers))
-        else alert("Please Enter A ")
-    };
-    console.log(charactersToBeUsed)
-    //if the boleon statement on confirmUpperCase is truthy, run the following function/method
-    if(userOptions.confirmUpperCase === true) {
-        characterChoice=characterChoice.concat(upperCase);
-        charactersToBeUsed.push(gettingRandomChar(upperCase))
-    };
-    console.log(userOptions)
-    //if the boleon statement on confirmLowerCase is truthy, run the following function/method
-    if(userOptions.confirmLowerCase === true) {
-       characterChoice=characterChoice.concat(lowerCase);
-       charactersToBeUsed.push(gettingRandomChar(lowerCase));
-    };
-    //for loop that takes the users input for password length and runs it through funtion for getting random characters
-    for (let index = 0; index < userOptions.confirmStrlength; index++) {
-       var characterChoice = gettingRandomChar(charactersToBeUsed)
-       chosenPW.push(characterChoice)
-    };
-    //for loop that takes the charactersToBeUsed and runs it through the chosenPW function to store into global memory
-    for (let index = 0; index < charactersToBeUsed.length; index++) {
-       chosenPW[index]=gettingRandomChar(charactersToBeUsed)
-        
-    };
-    return chosenPW.join('')
-       
+    //else-if statements if the user choses only three of the four confirm variables
+    else if (confirmChar && confirmNumber && confirmUpper) {
+        choices = character.concat(number, alpha2);
     }
-    //Function to generate wirte password parameter
-    function writePassword() {
-        var password = createPassword();
-        var passwordText = document.querySelector("#password");
-    
-        passwordText.value = password;
-    
-    
+    else if (confirmChar && confirmNumber && confirmLower) {
+        choices = character.concat(number, alpha);
+    }
+    else if (confirmChar && confirmLower && confirmUpper) {
+        choices = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLower && confirmUpper) {
+        choices = number.concat(alpha, alpha2);
+    }
+    //Else-if statement if the user choses only on of the four confirm variables
+    else if (confirmChar && confirmNumber) {
+        choices = character.concat(number);
+
+    } else if (confirmChar && confirmLower) {
+        choices = character.concat(alpha);
+
+    } else if (confirmChar && confirmUpper) {
+        choices = character.concat(alpha2);
+    }
+    else if (confirmLower && confirmNumber) {
+        choices = alpha.concat(number);
+
+    } else if (confirmLower && confirmUpper) {
+        choices = alpha.concat(alpha2);
+
+    } else if (confirmNumber && confirmUpper) {
+        choices = number.concat(alpha2);
+    }
+    //else-if statement if user choses only on of the confirm variables
+    else if (confirmChar) {
+        choices = character;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmLower) {
+        choices = alpha;
+    }
+    // Created upperCase variable to fill uppercase conversion
+    else if (confirmUpper) {
+        choices = upperCase.concat(alpha2);
     };
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+
+    //declaring a variable to global memory that is an array placeholder for user generated amount of length
+    var password = [];
+
+    //Randomzation for loop for selection of all variables: 
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+    //declaring a variable for global memory that joins the password variable and returns it as a string value
+    var ps = password.join("");
+    UserInput(ps);
+    return ps;
+}
+
+//function to take generated password and append it to the HTML document
+function UserInput(ps) {
+    document.getElementById("password").textContent = ps;
+
+}
